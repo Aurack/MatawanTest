b57fe6b909d29935e9bf2deff782fa4bd4974f6b
Aurack <thomas.bonin06@gmail.com>
2025-03-04 15:42:12 +0100

    First commit - Init project

---
+++ src/main/java/com/example/matawantest/MatawanTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.matawantest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatawanTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatawanTestApplication.class, args);
+	}
+
+}
