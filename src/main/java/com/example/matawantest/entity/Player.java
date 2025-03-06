58a1b056b73842437f09a4f9dff817c5026f6362
Aurack <thomas.bonin06@gmail.com>
2025-03-06 15:43:50 +0100

    Remove unused imports

--- src/main/java/com/example/matawantest/entity/Player.java
+++ src/main/java/com/example/matawantest/entity/Player.java
@@ -1,7 +1,6 @@
 package com.example.matawantest.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
